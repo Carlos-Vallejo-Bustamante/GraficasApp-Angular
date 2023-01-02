c0476b6ea239916ec6e6ea247ae2dac46197fe64
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-02 18:48:21 +0100

    first commit

---
+++ src/app/graficas/graficas.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraficasRoutingModule } from './graficas-routing.module';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDoblesComponent } from './pages/barras-dobles/barras-dobles.component';
+import { DonutComponent } from './pages/donut/donut.component';
+import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+import { GraficasComponent } from './services/graficas/graficas.component';
+
+
+@NgModule({
+  declarations: [
+    BarrasComponent,
+    BarrasDoblesComponent,
+    DonutComponent,
+    GraficaBarraComponent,
+    GraficasComponent
+  ],
+  imports: [
+    CommonModule,
+    GraficasRoutingModule
+  ]
+})
+export class GraficasModule { }
