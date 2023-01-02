3eda10bb0e6c082690b6ec60758fae6594508e13
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-02 20:40:57 +0100

    Add chart in graficaBarraComponent

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -6,11 +8,69 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class GraficaBarraComponent implements OnInit {
+export class GraficaBarraComponent {
 
-  constructor() { }
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  ngOnInit(): void {
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#5771F7', hoverBackgroundColor: '#AD6249' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#91F73E', hoverBackgroundColor: '#AD6249' },
+      { data: [8, 68, 50, 59, 16, 67, 100], label: 'Series C', backgroundColor: '#F75A25', hoverBackgroundColor: '#AD6249' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    // this.barChartData.datasets[0].data = [
+    //   Math.round(Math.random() * 100),
+    //   59,
+    //   80,
+    //   Math.round(Math.random() * 100),
+    //   56,
+    //   Math.round(Math.random() * 100),
+    //   40];
+
+    this.barChartData.datasets.map(bar => {
+      bar.data = [
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100)
+      ];
+    })
+
+    this.chart?.update();
   }
 
 }
