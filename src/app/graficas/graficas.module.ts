cec969b5d3bcaaa1c528c3678d5e6197f7e6eaeb
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-02 20:09:16 +0100

    Add Bar chart in barrasComponent

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgChartsModule } from 'ng2-charts';
+
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDoblesComponent } from './pages/barras-dobles/barras-dobles.component';
@@ -8,8 +10,6 @@ import { DonutComponent } from './pages/donut/donut.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
 import { GraficasComponent } from './services/graficas/graficas.component';
 import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
-
-
 @NgModule({
   declarations: [
     BarrasComponent,
@@ -21,7 +21,8 @@ import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    NgChartsModule
   ]
 })
 export class GraficasModule { }
