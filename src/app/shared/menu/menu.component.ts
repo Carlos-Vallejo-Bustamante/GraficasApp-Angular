ef1c4185c7a785c89526401d54e5d9ec57e90ec7
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-02 19:41:20 +0100

    Add Application menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
-})
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  styles: [`
+  li {
+    cursor: pointer;
   }
+  `]
+})
+export class MenuComponent {
+
+  menu: MenuItem[] = [
+    { ruta: '/graficas/barra', texto: 'Barras' },
+    { ruta: '/graficas/barra-doble', texto: 'Barras Dobles' },
+    { ruta: '/graficas/donut', texto: 'Donut' },
+    { ruta: '/graficas/donut-http', texto: 'Donut Http' }
+  ]
 
 }
