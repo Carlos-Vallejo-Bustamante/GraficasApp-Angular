f5b95aed20a86457f0efd35d9ba8adfca81e83fb
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-03 20:38:20 +0100

    Display the information received from http on the chart in donutHttpComponent

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -8,7 +8,6 @@ import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDoblesComponent } from './pages/barras-dobles/barras-dobles.component';
 import { DonutComponent } from './pages/donut/donut.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
-import { GraficasComponent } from './services/graficas/graficas.component';
 import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
 @NgModule({
   declarations: [
@@ -16,7 +15,6 @@ import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
     BarrasDoblesComponent,
     DonutComponent,
     GraficaBarraComponent,
-    GraficasComponent,
     DonutHttpComponent
   ],
   imports: [
--- src/app/graficas/pages/donut-http/donut-http.component.ts
+++ src/app/graficas/pages/donut-http/donut-http.component.ts
@@ -1,16 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-donut-http',
   templateUrl: './donut-http.component.html',
   styles: [
+
   ]
 })
 export class DonutHttpComponent implements OnInit {
 
-  constructor() { }
+  public doughnutChartLabels: string[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others'
+  ];
+  public dataSetsData: number[] = [];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: []
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
+    this.graficasService.getUsuariosRedesSociales()
+      .subscribe(data => {
+
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+
+        labels.forEach(element => {
+          this.doughnutChartLabels.push(element)
+        });
+
+        this.doughnutChartData.datasets.push(
+          { data: values, backgroundColor: ['#00D8ED', '#00A0F7', '#0B5DE0', '#01F0DB'], hoverBackgroundColor: '#4C00ED' }
+        );
+
+      })
   }
 
 }
