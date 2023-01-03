2a563f79bbb10147d8951526a3ce83efdd2eea51
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-03 12:44:55 +0100

    Add chart in DonutComponent

--- src/app/graficas/pages/donut/donut.component.ts
+++ src/app/graficas/pages/donut/donut.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-donut',
@@ -6,11 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonutComponent implements OnInit {
+export class DonutComponent {
+
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100, 150], backgroundColor: ['#00D8ED', '#00A0F7', '#0B5DE0', '#01F0DB'], hoverBackgroundColor: '#4C00ED' }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
