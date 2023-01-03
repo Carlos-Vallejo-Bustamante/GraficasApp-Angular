09b2a0a4f080f8518a877b77a0721d7ca1a7eeae
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-03 12:22:09 +0100

    Add graficaBarraComponent in barrasDoblesComponent

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -10,32 +10,21 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class GraficaBarraComponent {
 
+
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  @Input() indexAxis: "x" | "y" = 'x';
 
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10
-      }
-    },
-    plugins: {
-      legend: {
-        display: true,
-      }
-    }
   };
+
   public barChartType: ChartType = 'bar';
 
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#5771F7', hoverBackgroundColor: '#AD6249' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#91F73E', hoverBackgroundColor: '#AD6249' },
-      { data: [8, 68, 50, 59, 16, 67, 100], label: 'Series C', backgroundColor: '#F75A25', hoverBackgroundColor: '#AD6249' }
-    ]
+  @Input() barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: []
   };
 
   // events
@@ -71,6 +60,22 @@ export class GraficaBarraComponent {
     })
 
     this.chart?.update();
+  }
+
+  constructor() {
+    console.log(this.indexAxis);
+
+  }
+
+  ngOnInit(): void {
+
+    this.barChartData.datasets.map(value => {
+      if (value.indexAxis) {
+        value.indexAxis = this.indexAxis;
+      }
+
+    })
+
   }
 
 }
